3dee9f67a97d10100654697022624e3eee153994
jitendra jadav <jitendra.jadav@outlook.com>
2021-06-07 19:28:57 +0530

    Listview empty added

--- BharatSetu/BharatSetu/Views/FindByDistrictPage.xaml.cs
+++ BharatSetu/BharatSetu/Views/FindByDistrictPage.xaml.cs
@@ -19,5 +19,23 @@ namespace BharatSetu.Views
             base.OnAppearing();
             _viewModel.OnAppearing();
         }
+
+        private void ItemsListView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+        {
+
+            double scrollY = e.VerticalOffset < 0 ? 0 : e.VerticalOffset;
+            scrollY = scrollY > ItemsListView.Height ? ItemsListView.Height : scrollY;
+           
+            if (scrollY >= 50 && Shell.GetNavBarIsVisible(self) is true)
+            {
+                Shell.SetNavBarIsVisible(self, false);
+                Shell.SetFlyoutBehavior(self, FlyoutBehavior.Disabled);
+            }
+            if (scrollY == 0 && Shell.GetNavBarIsVisible(self) is false)
+            {
+                Shell.SetNavBarIsVisible(self, true);
+                Shell.SetFlyoutBehavior(self, FlyoutBehavior.Flyout);
+            }
+        }
     }
 }
